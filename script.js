5ba722bb9ab891aebf3e1faf7e08787cd66e7a96
Rittik Das <dassrittik1998@gmail.com>
2022-09-25 13:30:09 +0530

    Create style.css

---
+++ script.js
@@ -0,0 +1,41 @@
+let initialPrice = document.querySelector("#initial-price");
+let stockQuanity = document.querySelector("#quantity-stocks");
+let finalPrice = document.querySelector("#final-price");
+let submitButton = document.querySelector("#submit-btn");
+let output = document.querySelector("#output");
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    let loss = (initial - current) * quantity;
+    let lossPercentage = (loss / initial) * 100;
+
+    showOutput(`The loss is ${lossPercentage} % 🙁`);
+  } else if (initial < current) {
+    let profit = (current - initial) * quantity;
+    let profitPercentage = (profit / initial) * 100;
+    showOutput(`The profit percentage is ${profitPercentage} % 🤑`);
+  } else {
+    showOutput(`No profit,Hey no lose also!😇`);
+  }
+}
+
+let clickEventHandler = function () {
+  let initial = Number(initialPrice.value);
+
+  let quanityOfStocks = Number(stockQuanity.value);
+
+  let final = Number(finalPrice.value);
+
+  if (initial && quanityOfStocks && final) {
+    calculateProfitAndLoss(initial, quanityOfStocks, final);
+  } else {
+    showOutput(`Hey!!enter all the required fields.😡`);
+  }
+};
+
+function showOutput(message) {
+  output.innerText = message;
+}
+
+submitButton.addEventListener("click", clickEventHandler);
+// calculateProfitAndLoss(20, 2, 20);
